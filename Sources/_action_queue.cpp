4fda90c17b690b15dd42346164de0f0c3c0fa524
Steven W. Klassen <klassens@acm.org>
2018-12-20 17:40:41 -0700

    Updated the ActionQueue to use the latest version.

--- Sources/_action_queue.cpp
+++ Sources/_action_queue.cpp
@@ -2,88 +2,126 @@
 //  _action_queue.cpp
 //  kssio
 //
-//  Created by Steven W. Klassen on 2018-11-09.
+//  Created by Steven W. Klassen on 2018-12-20.
 //  Copyright © 2018 Klassen Software Solutions. All rights reserved.
 //  Licensing follows the MIT License.
 //
+// "borrowed" from kssthread
+//
 
-#include <atomic>
 #include <cassert>
-#include <cerrno>
+#include <atomic>
 #include <condition_variable>
 #include <exception>
 #include <iostream>
+#include <map>
+#include <mutex>
 #include <system_error>
 #include <thread>
-#include <vector>
 
 #include <syslog.h>
 
 #include "_action_queue.hpp"
+#include "_contract.hpp"
 
 using namespace std;
+using namespace std::chrono;
 using namespace kss::io::_private;
+namespace contract = kss::io::contract;
 
+using kss::io::_private::now;
+using time_point_t = time_point<steady_clock, milliseconds>;
+using action_map_t = multimap<time_point_t, ActionQueue::action_t>;
+
+
+// MARK: ActionQueue
 
 struct ActionQueue::Impl {
-    thread          actionThread;
+    std::thread     actionThread;
     size_t          maxPending = 0;
     atomic<bool>    stopping { false };
     atomic<bool>    waiting { false };
-    atomic<bool>    runningActions { false };
+    atomic<bool>    runningAction { false };
 
-    // The following are all protected by the lock and condition variable.
+    // The following must be protected by the lock and the condition variable.
     mutex               lock;
     condition_variable  cv;
-    vector<action_t>    pendingActions;
+    action_map_t        pendingActions;
 
     void runActionThread() {
-        vector<action_t> currentActions;
-        while (true) {
-            // While protected by the lock and condition variable, wait for there to be
-            // pending actions, then move them to a local list.
-            {
+         while (true) {
+             bool haveAction = false;
+             action_t currentAction;
+             time_point_t nextTargetTime;
+
+             // Wait until there is at least one item in the queue.
+             {
                 unique_lock<mutex> l(lock);
                 while (!stopping && pendingActions.empty()) { cv.wait(l); }
 
-                // If we are flagged to be stopping, break out of the loop so we exit
-                // the thread.
                 if (stopping) {
                     break;
                 }
 
-                // Dequeue any pending actions and run them.
-                assert(currentActions.empty());
                 if (!pendingActions.empty()) {
-                    currentActions.swap(pendingActions);
+                    // If the next action is due to run, remove it from the queue and
+                    // place it into currentAction. If it is not due to be run, compute
+                    // the duration that we should wait.
+                    const auto cit = pendingActions.begin();
+                    const auto currentTime = now<time_point_t>();
+                    if (cit->first <= currentTime) {
+                        runningAction = true;
+                        currentAction = move(cit->second);
+                        pendingActions.erase(cit);
+                        haveAction = true;
+                    }
+                    else {
+                        nextTargetTime = cit->first;
+                    }
                 }
-            }
+             }
 
-            // Run the items.
-            runningActions = true;
-            for (auto& action : currentActions) {
-                action();
-            }
-            currentActions.clear();
-            runningActions = false;
-            cv.notify_all();
-         }
+             // If we have an action to run, run it now. Otherwise we wait until the
+             // next target time has arrived.
+             if (haveAction) {
+                 currentAction();
+                 runningAction = false;
+                 cv.notify_all();
+             }
+             else {
+                 unique_lock<mutex> l(lock);
+                 cv.wait_until(l, nextTargetTime);
+             }
+        }
     }
 };
+
+
+const milliseconds ActionQueue::asap { 0 };
 
 ActionQueue::ActionQueue(ActionQueue&&) = default;
 ActionQueue& ActionQueue::operator=(ActionQueue &&) noexcept = default;
 
 ActionQueue::ActionQueue(size_t maxPending) : impl(new Impl()) {
     impl->maxPending = maxPending;
-    impl->actionThread = thread { [this]{ impl->runActionThread(); }};
+    impl->actionThread = std::thread { [this]{ impl->runActionThread(); }};
+
+    contract::postconditions({
+        KSS_EXPR(impl->maxPending == maxPending),
+        KSS_EXPR(impl->stopping == false),
+        KSS_EXPR(impl->waiting == false),
+        KSS_EXPR(impl->runningAction == false),
+        KSS_EXPR(impl->pendingActions.empty())
+    });
 }
 
 ActionQueue::~ActionQueue() noexcept {
     try {
         impl->stopping = true;
         impl->cv.notify_all();
-        impl->actionThread.join();
+        if (impl->actionThread.joinable()) {
+            impl->actionThread.join();
+        }
     }
     catch (const exception& e) {
         // Best we can do is log the error and continue.
@@ -91,39 +129,105 @@ ActionQueue::~ActionQueue() noexcept {
     }
 }
 
-void ActionQueue::addAction(const action_t &action) {
-    if (!impl->waiting && !impl->stopping) {
-        unique_lock<mutex> l(impl->lock);
-        if (impl->pendingActions.size() >= impl->maxPending) {
-            throw system_error(EAGAIN, system_category(), "addAction");
-        }
-        
-        impl->pendingActions.emplace_back(action);
-        impl->cv.notify_all();
-    }
-}
-
-void ActionQueue::addAction(action_t &&action) {
-    if (!impl->waiting && !impl->stopping) {
-        unique_lock<mutex> l(impl->lock);
-        if (impl->pendingActions.size() >= impl->maxPending) {
-            throw system_error(EAGAIN, system_category(), "addAction");
-        }
-        
-        impl->pendingActions.emplace_back(action);
-        impl->cv.notify_all();
-    }
-}
 
 void ActionQueue::wait() {
     unique_lock<mutex> l(impl->lock);
-    if (!impl->pendingActions.empty() || impl->runningActions) {
+    if (!impl->pendingActions.empty() || impl->runningAction) {
         impl->waiting = true;
-        while (!impl->stopping
-               && (!impl->pendingActions.empty() || impl->runningActions))
-        {
+        while (!impl->stopping && (!impl->pendingActions.empty() || impl->runningAction)) {
             impl->cv.wait(l);
         }
         impl->waiting = false;
+    }
+
+    contract::postconditions({
+        KSS_EXPR(impl->waiting == false),
+        KSS_EXPR(impl->runningAction == false),
+        KSS_EXPR(impl->pendingActions.empty())
+    });
+}
+
+
+void ActionQueue::addActionAfter(const milliseconds &delay, const action_t &action) {
+    contract::parameters({
+        KSS_EXPR(delay.count() >= 0)
+    });
+
+    if (!impl->stopping) {
+        if (impl->waiting) {
+            throw system_error(EAGAIN, system_category(), "addActionAfter (queue waiting)");
+        }
+        else {
+            unique_lock<mutex> l(impl->lock);
+            if (impl->pendingActions.size() >= impl->maxPending) {
+                throw system_error(EAGAIN, system_category(), "addActionAfter");
+            }
+
+            auto targetTime = now<time_point_t>() + delay;
+            impl->pendingActions.emplace(targetTime, action);
+
+            contract::postconditions({
+                KSS_EXPR(!impl->pendingActions.empty())
+            });
+            impl->cv.notify_all();
+        }
+    }
+}
+
+void ActionQueue::addActionAfter(const milliseconds &delay, action_t &&action) {
+    contract::parameters({
+        KSS_EXPR(delay.count() >= 0)
+    });
+
+    if (!impl->stopping) {
+        if (impl->waiting) {
+            throw system_error(EAGAIN, system_category(), "addActionAfter (queue waiting)");
+        }
+        else {
+            unique_lock<mutex> l(impl->lock);
+            if (impl->pendingActions.size() >= impl->maxPending) {
+                throw system_error(EAGAIN, system_category(), "addActionAfter");
+            }
+
+            auto targetTime = now<time_point_t>() + delay;
+            impl->pendingActions.emplace(targetTime, move(action));
+
+            contract::postconditions({
+                KSS_EXPR(!impl->pendingActions.empty())
+            });
+            impl->cv.notify_all();
+        }
+    }
+}
+
+
+// MARK: RepeatingAction
+
+RepeatingAction::~RepeatingAction() noexcept {
+    stopping = true;
+    while (!stopped) {
+        std::this_thread::sleep_for(10ms);
+    }
+}
+
+void RepeatingAction::runActionAndRequeue() {
+    if (stopping) {
+        stopped = true;
+    }
+    else {
+        action();
+        try {
+            queue.addAction(timeInterval, internalAction);
+        }
+        catch (const std::system_error& err) {
+            // If the queue is currently paused or full, we pause, then try again.
+            if (err.code() == error_code(EAGAIN, system_category())) {
+                this_thread::sleep_for(timeInterval);
+                internalAction();
+            }
+            else {
+                throw err;
+            }
+        }
     }
 }
--- Sources/_action_queue.hpp
+++ Sources/_action_queue.hpp
@@ -2,74 +2,183 @@
 //  _action_queue.hpp
 //  kssio
 //
-//  Created by Steven W. Klassen on 2018-11-09.
+//  Created by Steven W. Klassen on 2018-12-20.
 //  Copyright © 2018 Klassen Software Solutions. All rights reserved.
 //  Licensing follows the MIT License.
+//
+// Although this version is created more recently (2018) it is largely based on earlier
+// legacy versions of kssutil and kssthread. Note that it is intended to replace the
+// somewhat broken dispatch class, which is now marked as deprecated.
+//
+//    kssutil thread.h (2002)
+//    kssutil timer.h (2003)
+//    kssthread dispatch.h (2014)
+//
+// "borrowed" from kssthread
 //
 
 #ifndef kssio_action_queue_hpp
 #define kssio_action_queue_hpp
 
+#include <atomic>
+#include <chrono>
 #include <functional>
 #include <limits>
 #include <memory>
+#include <thread>
+#include "utility.hpp"
 
-namespace kss {
-    namespace io {
-        namespace _private {
+namespace kss { namespace io { namespace _private {
 
-            // "Borrowed" from kssthread
+    /*!
+     An action queue is used to allow a variety of actions to be queued on a single thread.
+     Each action, added by one of the addAction() methods, will be run as soon as possible
+     after its target delay. If the delay is the value asap (or any other duration that maps
+     to zero) the action will be run as soon as possible.
 
-            /*!
-             An action queue provides a single-thread processing queue that performs
-             the actions it is given in order. As long as actions are pending, it will
-             perform them. When it runs out the thread will sleep until more actions
-             are added.
-             */
-            class ActionQueue {
-            public:
-                using action_t = std::function<void()>;
-                static constexpr size_t noLimit = std::numeric_limits<size_t>::max();
+     The key advantage of this over, for example, a thread looping with a wait_for() call,
+     is that multiple timed things can be added, all shared with a single thread. In
+     particular, if you have two or three threads that each loop and call wait_for(), you
+     can replace them with a single instance of this class (and its single thread) and
+     its helpers. (See TimedActionGenerator.)
 
-                /*!
-                 Construct an action queue.
-                 @param maxPending puts a maximum limit on the number of pending actions.
-                 */
-                explicit ActionQueue(size_t maxPending = noLimit);
+     Another use of an action queue is to serialize accesses to some resource. That is,
+     instead of accessing your resource directly, add actions that access it to the
+     action queue. Since the action queue operates in a single thread, this ensures that
+     the access will always be serialized without the need to managing locks. (Or more
+     accurately, by relying on the ActionQueue internal locks.)
+     */
+    class ActionQueue {
+    public:
+        using action_t = std::function<void()>;
 
-                ActionQueue(ActionQueue&&);
-                ActionQueue& operator=(ActionQueue&&) noexcept;
-                ~ActionQueue() noexcept;
+        /*!
+         Use this in the constructor to specify that you do not want to enforce a limit
+         on the number of items in the queue.
+         */
+        static constexpr size_t noLimit = std::numeric_limits<size_t>::max();
 
-                ActionQueue(const ActionQueue&) = delete;
-                ActionQueue& operator=(const ActionQueue&) = delete;
+        /*!
+         Use this in addAction() to specify that you want the action to be performed as
+         soon as possible. This makes the queue work similar to ActionQueue.
+         */
+        static const std::chrono::milliseconds asap;
 
-                /*!
-                 Add an action to the queue. The action will be performed as soon as
-                 possible.
-                 @throws std::system_error with a value of EAGAIN if the action queue
-                    already has maxPending items, or if we are currently waiting for
-                    pending actions to complete. In either case it is recommended that
-                    the caller wait a short time, then try again.
-                 @throws any exceptions that a condition_variable or a vector may throw.
-                 */
-                void addAction(const action_t& action);
-                void addAction(action_t&& action);
 
-                /*!
-                 Wait util all pending actions have completed. Note that no further actions
-                 may be added while waiting. But they may be added as soon as wait()
-                 has returned.
-                 @throws any exceptions that a condition_variable or a vector may throw
-                 */
-                void wait();
+        /*!
+         Construct the queue.
+         @param maxPending puts a maximum limit on the number of pending actions.
+         */
+        explicit ActionQueue(size_t maxPending = noLimit);
 
-            private:
-                struct Impl;
-                std::unique_ptr<Impl> impl;
-            };
+        ActionQueue(ActionQueue&&);
+        ActionQueue& operator=(ActionQueue&&) noexcept;
+        ~ActionQueue() noexcept;
+
+        ActionQueue(const ActionQueue&) = delete;
+        ActionQueue& operator=(const ActionQueue&) = delete;
+
+        /*!
+         Add an action to the queue. The action will be performed as soon as possible after
+         the given delay.
+         @throws std::invalid_argument if the delay is a negative value
+         @throws std::system_error with a value of EAGAIN if the action queue
+            already has maxPending items, or if we are currently waiting for
+            pending actions to complete. In either case it is recommended that
+            the caller wait a short time, then try again.
+         @throws any exceptions that a condition_variable, a map, or a
+            checked_duration_cast may throw.
+         */
+        template <class Duration>
+        inline void addAction(const Duration& delay, const action_t& action) {
+            using kss::io::_private::checked_duration_cast;
+            addActionAfter(checked_duration_cast<std::chrono::milliseconds>(delay), action);
         }
-    }
-}
 
-#endif /* _action_queue_hpp */
+        template <class Duration>
+        inline void addAction(const Duration& delay, action_t&& action) {
+            using kss::io::_private::checked_duration_cast;
+            addActionAfter(checked_duration_cast<std::chrono::milliseconds>(delay), action);
+        }
+
+        /*!
+         Add an action to the queue. The action will be performed as soon as possible.
+         (I.e. It will be given a delay of 0.)
+         @throws std::invalid_argument if the delay is a negative value
+         @throws std::system_error with a value of EAGAIN if the action queue
+            already has maxPending items, or if we are currently waiting for
+            pending actions to complete. In either case it is recommended that
+            the caller wait a short time, then try again.
+         @throws any exceptions that a condition_variable or a map may throw.
+         */
+        inline void addAction(const action_t& action) {
+            addActionAfter(asap, action);
+        }
+
+        inline void addAction(action_t&& action) {
+            addActionAfter(asap, action);
+        }
+
+        /*!
+         Wait util all pending actions have completed. Note that no further actions
+         may be added while waiting. But they may be added as soon as wait()
+         has returned.
+         @throws any exceptions that a condition_variable or a map may throw
+         */
+        void wait();
+
+    private:
+        struct Impl;
+        std::unique_ptr<Impl> impl;
+
+        void addActionAfter(const std::chrono::milliseconds& delay, const action_t& action);
+        void addActionAfter(const std::chrono::milliseconds& delay, action_t&& action);
+    };
+
+
+    /*!
+     A repeating action is a helper class useful when you want to repeat the same action
+     at regular intervals. You give it the desired interval, an ActionQueue, and the action,
+     and it will repeatedly add the action with the given interval to the queue. It will
+     continue to do this until it goes out of scope.
+
+     Note that it is important that the ActionQueue remain in scope for at least as long
+     as the RepeatingAction instance.
+     */
+    class RepeatingAction {
+    public:
+        template <class Duration>
+        RepeatingAction(const Duration& interval,
+                        ActionQueue& q,
+                        const ActionQueue::action_t& act)
+        : timeInterval(kss::io::_private::checked_duration_cast<std::chrono::milliseconds>(interval)),
+        queue(q), action(act)
+        {
+            queue.addAction(timeInterval, internalAction);
+        }
+
+        template <class Duration>
+        RepeatingAction(const Duration& interval,
+                        ActionQueue& q,
+                        ActionQueue::action_t&& act)
+        : timeInterval(kss::io::_private::checked_duration_cast<std::chrono::milliseconds>(interval)),
+        queue(q), action(act)
+        {
+            queue.addAction(timeInterval, internalAction);
+        }
+
+        ~RepeatingAction() noexcept;
+
+    private:
+        std::chrono::milliseconds   timeInterval;
+        ActionQueue&                queue;
+        ActionQueue::action_t       action;
+        ActionQueue::action_t       internalAction = [this] { runActionAndRequeue(); };
+        std::atomic<bool>           stopping { false };
+        std::atomic<bool>           stopped { false };
+
+        void runActionAndRequeue();
+    };
+}}}
+
+#endif
